922a2fe94accbaf1bf6a3b610ef493497b90571b
chandrabcs <117516514+chandrabcs@users.noreply.github.com>
2023-07-27 17:18:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+document.querySelector(".my-image").addEventListener("",()=>{
+    alert("hgf");
+})
+
+document.querySelector("h1").addEventListener("click", ()=> {
+    let a = confirm("error");
+    // console.log(this.innerHTML);
+    // document.querySelectorAll("h3").innerHTML="<b>Hey</b>";
+})
+
+// document.addEventListener("keypress",(e)=>
+// {
+//     console.log(e.key);
+// })
